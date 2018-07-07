476ab7927a1c77337aa8fb3db11d825e1700cc69
FireInjun <mk@fireinjun.com>
2018-07-06 23:59:47 -0400

    notes routes and models added

---
+++ notes/NotesModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const NotesSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  created: {
+    type: Date, default: Date.now
+  },
+  author: {
+    default: "Public",
+    type: String
+  }
+});
+
+module.exports = mongoose.model("Note", NotesSchema);
